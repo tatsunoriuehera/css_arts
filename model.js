c904d2200003258d260326bf510cf17d208ec4db
tatsunoriuehera <42107566+tatsunoriuehera@users.noreply.github.com>
2018-11-01 15:14:54 +0900

    Add files via upload

---
+++ model.js
@@ -0,0 +1,18 @@
+function ohayou(){
+window.alert("hello!!");
+}
+
+function oyasumi(){
+window.alert("good night");
+}
+
+function opengoogle(){
+	window.open("http://www.google.co.jp/", "Google");
+}
+
+/* タイマーによるポップアップ ３秒後に、opengoogleのjavascriptを実行する
+function opengoogle(){
+	window.open("http://www.google.co.jp/", "Google");
+}
+setTimeout("opengoogle()",3000);
+*/
